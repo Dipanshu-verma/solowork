30b66e5e136aa0fe1820c48c6bbd0888bf69ad89
Dipanshu-verma <vermadipanshu444@gmail.com>
2023-07-09 20:00:39 +0530

    treval

--- treval/src/App.js
+++ treval/src/App.js
@@ -8,9 +8,10 @@ function App() {
   return (
     <div className="App">
     <Navbar/>
-    <Main/>
-    <Footer/>
     <Home/>
+    <Main/>
+    {/* <Footer/> */}
+  
     </div>
   );
 }
--- treval/src/Component/Home/Home.jsx
+++ treval/src/Component/Home/Home.jsx
@@ -1,22 +1,70 @@
-import React from 'react'
-import './home.css'
-import video from '../../Assest/beach.mp4'
+import React from "react";
+import "./home.css";
+import video from "../../Assest/beach.mp4";
+import "./home.scss";
+import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { SiTripadvisor } from "react-icons/si";
+import { BsListTask } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
+
+
+
 export default function Home() {
   return (
-    <section className='home'>
-      <div className='overlay'></div>
-      <video src={video} muted autoPlay loop type="video/mp4"> </video>
-      <div className='homeContent container'>
-        <div className='textDiv'>
-      <span className='smallText'>
-        Our Packeges
-      </span>
-      <h1 className='homeTitle'>
-      Search Your Holiday
+    <section className="home">
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop type="video/mp4">
+        {/* {" "} */}
+      </video>
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallText">Our Packeges</span>
+          <h1 className="homeTitle">Search Your Holiday</h1>
+        </div>
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Search Your destination:</label>
+            <div className="input flex">
+              <input type="text" placeholder="Enter Your destination" />
+              <GrLocation className="icon" />
+            </div>
+          </div>
+          <div className="dateInput">
+            <label htmlFor="date">Select Your Date</label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+          <div className="priceInput">
+            <div className="label_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className="total">$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000" />
+            </div>
+          </div>
+          <div className="searchOptions flex">
+            <HiFilter className="icon"/>
+            <span>More Filter</span>
+          </div>
+        </div>
 
-      </h1>
+        <div className="homeFooterIcons  flex">
+        <div className="rightIcons">
+         <FiFacebook className='icon'/>
+         <AiOutlineInstagram className='icon'/>
+         <SiTripadvisor className='icon'/>
+        </div>
+        <div className="leftIcons">
+        <BsListTask className='icon'/>
+        <TbApps className='icon'/>
+</div>
         </div>
       </div>
     </section>
-  )
+  );
 }
