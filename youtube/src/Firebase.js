2a08f22133c44d6df65f73c6dd253957887318b4
Dipanshu-verma <vermadipanshu444@gmail.com>
2023-08-18 21:52:20 +0530

    yt

--- youtube/src/Firebase.js
+++ youtube/src/Firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBCk6jR4lQIEshZCWix7rjk9OutEGzh2Is",
-//   authDomain: "tube-you-pro.firebaseapp.com",
-//   projectId: "tube-you-pro",
-//   storageBucket: "tube-you-pro.appspot.com",
-//   messagingSenderId: "155298825875",
-//   appId: "1:155298825875:web:c2402c75b499f3b799ef66"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBCk6jR4lQIEshZCWix7rjk9OutEGzh2Is",
+  authDomain: "tube-you-pro.firebaseapp.com",
+  projectId: "tube-you-pro",
+  storageBucket: "tube-you-pro.appspot.com",
+  messagingSenderId: "155298825875",
+  appId: "1:155298825875:web:c2402c75b499f3b799ef66"
+};
 // const firebaseConfig = {
 //   apiKey: "AIzaSyDMj6YRra2Vnq-biKv3SpDl3yn6_XZ0duA",
 //   authDomain: "clone-of-yt.firebaseapp.com",
@@ -35,14 +35,14 @@ import 'firebase/compat/auth';
 //   appId: "1:283814602509:web:82064cde7a9b50d755f244"
 // };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAVha1YR7Nrq_nr7qRZtQ5e94E-LMkD4yo",
-  authDomain: "my-project-of-yt.firebaseapp.com",
-  projectId: "my-project-of-yt",
-  storageBucket: "my-project-of-yt.appspot.com",
-  messagingSenderId: "746333407799",
-  appId: "1:746333407799:web:0ca87425fb69416be24db2"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAVha1YR7Nrq_nr7qRZtQ5e94E-LMkD4yo",
+//   authDomain: "my-project-of-yt.firebaseapp.com",
+//   projectId: "my-project-of-yt",
+//   storageBucket: "my-project-of-yt.appspot.com",
+//   messagingSenderId: "746333407799",
+//   appId: "1:746333407799:web:0ca87425fb69416be24db2"
+// };
 
   firebase.initializeApp(firebaseConfig)
   export default firebase.auth();
--- youtube/src/api.js
+++ youtube/src/api.js
@@ -4,7 +4,7 @@ const request  = axios.create({
     baseURL : 'https://youtube.googleapis.com/youtube/v3/',
 
     params:{
-        key:"AIzaSyAVha1YR7Nrq_nr7qRZtQ5e94E-LMkD4yo",
+        key:"AIzaSyBCk6jR4lQIEshZCWix7rjk9OutEGzh2Is",
     },
 
 })
